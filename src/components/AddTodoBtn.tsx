33ec82599b5bba37b28aad977a24ce153724e69e
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-02-02 00:36:30 +0100

    Add todo button UI

---
+++ src/components/AddTodoBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTodoBtn = () => {
+    return (
+        <div className='flex justify-center'>
+            <button className='shadow-lg shadow-indigo-300 text-indigo-500 font-bold w-14 h-14 flex items-center justify-center rounded-full text-3xl'>+</button>
+        </div>
+    )
+}
+
+export default AddTodoBtn
--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TodoDay from './TodoTime'
 import TodoList from './TodoList'
+import AddTodoBtn from './AddTodoBtn'
 
 const TodoCard = () => {
     return (
-        <section className="p-4 bg-white max-w-xl w-full flex flex-col gap-12">
+        <section className="px-4 py-8 bg-white max-w-xl w-full flex flex-col gap-12 shadow-lg rounded-md">
             <TodoDay />
             <TodoList />
+            <AddTodoBtn />
         </section>
     )
 }
